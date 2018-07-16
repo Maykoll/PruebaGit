ed4eea3b6b7e8b9df2caafab2fe3c45dd0d7d01d
Maycol <maycol.gonzalez7@gmail.com>
2018-07-16 00:42:11 -0400

    Primer commit

---
+++ AporteDiego/src/aportediego/Probando.java
@@ -0,0 +1,42 @@
+
+package aportediego;
+
+
+public class Probando {
+    
+    public String nombre;
+    public String apellido;
+    public int id;
+    
+    Probando(){
+    
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    
+    
+    
+}
