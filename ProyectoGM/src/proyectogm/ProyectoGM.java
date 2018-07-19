24824106db92c927d252d5f7d001d832825c0ee7
Dgocamp <38407836+Dgocamp@users.noreply.github.com>
2018-07-18 21:16:59 -0400

    aporte a gm

--- ProyectoGM/src/proyectogm/ProyectoGM.java
+++ ProyectoGM/src/proyectogm/ProyectoGM.java
@@ -17,6 +17,7 @@ public class ProyectoGM {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Aporte GM");
+        System.out.println("Segundo Aporte GM");
     }
     
 }
