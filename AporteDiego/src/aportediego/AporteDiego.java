c1ed41565cf1c94b186dd5633642ba00c6f05722
Dgocamp <38407836+Dgocamp@users.noreply.github.com>
2018-07-18 20:08:23 -0400

    aporte

--- AporteDiego/src/aportediego/AporteDiego.java
+++ AporteDiego/src/aportediego/AporteDiego.java
@@ -25,7 +25,6 @@ public class AporteDiego {
         System.out.println("Gm reportandose y aportando jajaja");
         System.out.println("Aportando info");
         System.out.println("Aporte extra");
-        System.out.println("Aporte adicional");
       
     }
     
